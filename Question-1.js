0420a817142074ff09a0234d861cd2075cd68392
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-20 08:41:10 +0530

    Question 1 & 2 Updated

--- Question-1.js
+++ Question-1.js
@@ -7,3 +7,15 @@
 /* What is JavaScript- JavaScript (JS) is the world’s most popular lightweight, interpreted compiled 
 programming language. It is also known as a scripting language for web pages. It can be used for 
 Client-side as well as Server-side developments.*/
+
+
+/*How does it work- 
+
+
+                                                        / ==>   Conversion to Machine Code             
+                                                      /                 ||
+JavaScropt File==>Praser==>AST(abstract syntax tree)/           Converted Machne Code
+                                                                        ||
+                                                                     Code RUN
+
+*/
