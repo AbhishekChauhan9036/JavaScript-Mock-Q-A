e46c701cab34ea6147f96bb69d0f2c1a2eb50185
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-20 08:33:33 +0530

    Question 1 & 2 Updated

--- Question-1.js
+++ Question-1.js
@@ -1,7 +1,9 @@
 // Mock SET-1 Interview Question 1 - What is javascript? How does it work?
 
+
 // Video Summary:- https://drive.google.com/file/d/1mSxhfouCBKlyg9WYR33Jmy8pB79psfFd/view?usp=sharing
 
-// What is JavaScript-
 
-
+/* What is JavaScript- JavaScript (JS) is the world’s most popular lightweight, interpreted compiled 
+programming language. It is also known as a scripting language for web pages. It can be used for 
+Client-side as well as Server-side developments.*/
