47287b09a869d7329227dd3efd9a24821b7e66d9
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2022-12-20 08:22:30 +0530

    Question 1 & 2 Updated

--- Question-1.js
+++ Question-1.js
@@ -1,7 +1,7 @@
 // Mock SET-1 Interview Question 1 - What is javascript? How does it work?
 
-Video Summary:- 
+// Video Summary:- https://drive.google.com/file/d/1mSxhfouCBKlyg9WYR33Jmy8pB79psfFd/view?usp=sharing
 
-What is JavaScript-
+// What is JavaScript-
 
 
--- Question-2.js
+++ Question-2.js
@@ -1,1 +1,16 @@
 // Mock SET-1 Interview Question 2 - Difference between var, let and const?
+
+// Video Summary:- https://drive.google.com/file/d/19UZJH1jCHC4ZG9whI8QTK2semO6pahEs/view?usp=sharing
+
+function start() {
+  for (let i = 0; i < 5; i++) {
+    // i=10;
+    console.log(i);
+  }
+  //   console.log(i);
+}
+
+start();
+
+// let,const => Block Scope
+//var =>function Scope
